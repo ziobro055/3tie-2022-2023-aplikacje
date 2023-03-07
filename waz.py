3752f6c5d0ea208e8355ec858541f0758030c3dc
ziobro055 <113183282+ziobro055@users.noreply.github.com>
2023-03-07 14:59:28 +0100

    Update waz.py

--- waz.py
+++ waz.py
@@ -8,6 +8,7 @@ def waz():
     pygame.init()
     #utworzenie okna gry i okreslenie jego rozmiarów
     oknoGry=pygame.display.set_mode((600,600),0,32)
+   
     #ustawiamy nazwę okienka
     pygame.display.set_caption("Gra Wąż")
     #tworzymy zmienną, która przechowuje informacje czy gra jest uruchomiona
@@ -18,10 +19,12 @@ def waz():
     #pozycja startowa jabłka
     jablkoX=random.randint(0,19)*30
     jablkoY=random.randint(0,19)*30
+
+    punkty=0
     #pętla while sprawdza czy warunek w zmiennej run jest prawdziwy, jak jest nieprawdziwy kończy swoje działanie
     while(run):
         #Wypełnienie okna kolorem
-        oknoGry.fill((0,0,0))
+        oknoGry.fill((229,255,204))
         #ustawienie opóźnienia odświeżania gry
         pygame.time.delay(200)
         #sprawdzanie czy istnieją jakieś zdarzenia i zapisanie ich do zmiennej "zdarzenia"
@@ -39,18 +42,29 @@ def waz():
                     zmiennaY=zmiennaY-30
                 elif zdarzenia.key==pygame.K_DOWN:
                     zmiennaY=zmiennaY+30
+                #sprawdzanie krawedzi
+                if zmiennaX>600:
+                    zmiennaX=0
+                if zmiennaX<0:
+                    zmiennaX=600
+                    
+            
         #rysowanie węża
         #definiujemy kształ węża
         ksztaltWaz=pygame.Rect((zmiennaX,zmiennaY),(30,30))
         #dodanie kształtu do okienka
         pygame.draw.rect(oknoGry,(100,100,100),ksztaltWaz)
-        #zjadanie jabłka
+ #zjadanie jablka
         if zmiennaX==jablkoX and zmiennaY == jablkoY:
             jablkoX=random.randint(0,19)*30
             jablkoY=random.randint(0,19)*30
-
+            punkty+=1 #punkty=punkty+1
         #rysowanie jabłka
         pygame.draw.circle(oknoGry,(255,0,0),(jablkoX+15,jablkoY+15),15)
+        #napisy
+        czcionka=pygame.font.SysFont('arial',25)
+        tekst=czcionka.render("zdobyte puknty:{0}".format(punkty),1,(51,51,255))
+        oknoGry.blit(tekst,(10,10))
         #aktualizowanie zawartości okna gry
         pygame.display.update()
 
